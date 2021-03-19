df838c9c077c964517c2c8af02f16d39cb994d34
John Sanabria <john.sanabria@correounivalle.edu.co>
2021-03-19 17:15:40 +0000

    fork con wait

--- fork-main.c
+++ fork-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 int main(int argc, char** argv) {
@@ -16,8 +17,10 @@ int main(int argc, char** argv) {
      a = a + 10; // hijo
      printf("Soy el hijo %d y el valor de a es: %d\n",getpid(),a);
    } else {
+     wait(NULL);
      a = a + 5; // padre
      printf("Soy el padre %d y el valor de a es: %d\n",getpid(),a);
    }
+   printf("Mi pid es %d\n",getpid());
    return 0;
 }
